392b730251ca2224b7108123efb4dcdaa20c6ca0
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2023-05-05 00:35:35 +0530

    Create Project Details.js

---
+++ Project Details.js
@@ -0,0 +1,9 @@
+HLD Design:- https://miro.com/app/board/uXjVMdEdTCo=/?share_link_id=238804135953
+PRD:- https://docs.google.com/document/d/1jz84IkpBWmn1ifoc_27MXZAlW9GVjOiwFgnZs1KTL8E/edit#
+Project Live Link:- https://fashion-fusion.netlify.app
+Source Code:-
+Frontend:-https://github.com/pesto-students/fashionfusion-frontend-team2-nitish
+Backend:-https://github.com/pesto-students/fashionfusion-backend-team2-nitish
+Wireframes:-https://www.figma.com/file/B9b23IHdqfsZrfzz9rg3CC/Fashion-Fusion-Desktop-Design?node-id=0%3A1&t=SUxysB2gAWCNO3By-1
+Desktop:-https://www.figma.com/file/B9b23IHdqfsZrfzz9rg3CC/Fashion-Fusion-Desktop-Design?node-id=0%3A1&t=SUxysB2gAWCNO3By-1
+Mobile:-https://www.figma.com/file/m5fA6EUm8X2PhNBXOBfORY/Fashion-Fusion-Mobile-Design?node-id=0%3A1&t=WMnc8Vl5w6uvkTlC-1
